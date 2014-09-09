da90a33e78146adb42a85576f5ef9adb894bb975
Mark Hanson <mhanson01@gmail.com>
2014-09-09 12:20:51 -0400

    improved comments

--- public/index.php
+++ public/index.php
@@ -1,15 +1,15 @@
 <?php
 
+use Maha\Core\IoC;
+
 //todo: move more settings into bootstrap or create a start.php to utilize
 
-/**
- * Autoload composer and run app
- */
-
-use Maha\Core\IoC;
+/** Bootstrap the app */
 
 require '../src/Maha/bootstrap.php';
 
+/** Create the app from the IoC container */
 $app = IoC::make('app');
 
+/** Run the app */
 $app->run();
