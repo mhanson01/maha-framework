e6c662ef1650311253d940b2e4a1ed29aca4c78f
Mark Hanson <mhanson01@gmail.com>
2014-09-09 22:19:42 -0400

    can now respond to additional http methods

--- src/Maha/Core/Request.php
+++ src/Maha/Core/Request.php
@@ -17,15 +17,29 @@ class Request {
      */
     public $method;
 
+    private $http_methods = [
+        'GET',
+        'POST',
+        'PUT',
+        'PATCH',
+        'DELETE'
+    ];
+
     /**
      *
      */
     function __construct()
     {
         $this->uri = $_SERVER['REQUEST_URI'];
+        if( isset($_REQUEST['_method']) and in_array($_REQUEST['_method'], $this->http_methods) )
+        {
+            $this->method = $_REQUEST['_method'];
+        }
+        else
+        {
+            $this->method = $_SERVER['REQUEST_METHOD'];
 
-        // todo: check for hidden variables to simulate put, patch, delete requests
-        $this->method = $_SERVER['REQUEST_METHOD'];
+        }
     }
 
 
